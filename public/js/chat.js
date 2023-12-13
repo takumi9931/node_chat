d031139b5bda0de699cf0356437003957b8eefec
takumi <220022@yse-c.net>
2023-12-13 11:46:01 +0900

    aaa

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,16 @@
-
+// Chatサーバに接続
+const URL = "";
+const socket = io.connect(URL); 
 
 function sendMessage() {
     // console.log('send message!!')
     var message = document.getElementById('message').value;
     console.log(message);
+    if (message) {
+        //データを作成
+        var data = { 
+            message: message
+        }
+        socket.emit('chat_message', data);
+    }
 }
